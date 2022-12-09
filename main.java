b4643f12c25f0c6fb3f082e8f971ed6cf65a252e
/Peter <114913947+PeterVillanueva@users.noreply.github.com>
2022-12-09 21:14:09 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+public class NestedIfExample { 
+  
+  public static void main(String args[]) {
+    
+    int num=500;
+    if (num < 600) {
+    System.out.println("number is less than 600");
+  
+    if (num > 350) {
+      System.out.println("number is greater than 350");
+    }
+  }
+  }
+}
